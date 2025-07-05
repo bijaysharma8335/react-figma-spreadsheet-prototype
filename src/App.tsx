12139b90e4b58f71abdea20dfc7427437f53a50a
Bijay Sharma <58084675+bijaysharma8335@users.noreply.github.com>
2025-07-06 02:41:56 +0530

    chore: initialize Tailwind CSS 3.4.3 with Vite + React

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <h1 className="text-3xl text-green-900 font-bold">React Figma Spreadsheet Prototype</h1>
+        </>
+    );
+}
+
+export default App;

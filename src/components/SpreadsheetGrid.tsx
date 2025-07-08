331f9720c944f6f493e296c909c6a2e8005fa5ee
Bijay Sharma <58084675+bijaysharma8335@users.noreply.github.com>
2025-07-08 19:03:57 +0530

    fix: console errors  and react-table dependency errors in spreadsheet

--- src/components/SpreadsheetGrid.tsx
+++ src/components/SpreadsheetGrid.tsx
@@ -3,16 +3,8 @@ import { FiChevronDown } from "react-icons/fi";
 import { IoChevronDownCircle, IoLinkOutline } from "react-icons/io5";
 import { FaArrowsRotate } from "react-icons/fa6";
 import { IoMdAdd } from "react-icons/io";
-import {
-    useTable,
-    Column,
-    HeaderGroup,
-    Row,
-    Cell,CellProps,
-    ColumnInstance,
-    useResizeColumns,
-    useBlockLayout,
-} from "react-table";
+import type { Column, HeaderGroup, Row, Cell, CellProps, ColumnInstance } from "react-table";
+import { useTable } from "react-table";
 
 import bag from "../assets/bag.svg";
 import submit from "../assets/submit.svg";
@@ -49,7 +41,7 @@ const generateData = (): RowData[] => {
             assigned: "Sophie Choudhury",
             priority: "Medium",
             dueDate: "20-11-2024",
-            estValue: "₹6,200,000",
+            estValue: "6,200,000",
             last: "",
         },
         {
@@ -61,7 +53,7 @@ const generateData = (): RowData[] => {
             assigned: "Tejas Pandey",
             priority: "High",
             dueDate: "30-10-2024",
-            estValue: "₹3,500,000",
+            estValue: "3,500,000",
             last: "",
         },
         {
@@ -73,7 +65,7 @@ const generateData = (): RowData[] => {
             assigned: "Rachel Lee",
             priority: "Medium",
             dueDate: "10-12-2024",
-            estValue: "₹4,750,000",
+            estValue: "4,750,000",
             last: "",
         },
         {
@@ -85,7 +77,7 @@ const generateData = (): RowData[] => {
             assigned: "Tom Wright",
             priority: "Low",
             dueDate: "15-01-2025",
-            estValue: "₹5,900,000",
+            estValue: "5,900,000",
             last: "",
         },
         {
@@ -97,7 +89,7 @@ const generateData = (): RowData[] => {
             assigned: "Kevin Smith",
             priority: "Low",
             dueDate: "30-01-2025",
-            estValue: "₹2,800,000",
+            estValue: "2,800,000",
             last: "",
         },
     ];
@@ -137,7 +129,7 @@ const columns: Column<RowData>[] = [
             </div>
         ),
         accessor: "jobRequest",
-        Cell: ({ value } :CellProps<RowData, string>)=> (
+        Cell: ({ value }: CellProps<RowData, string>) => (
             <div className="text-gray-700">{value || <span className="text-gray-300">-</span>}</div>
         ),
     },
@@ -155,7 +147,7 @@ const columns: Column<RowData>[] = [
             </div>
         ),
         accessor: "submitted",
-       Cell: ({ value } :CellProps<RowData, string>)=> (
+        Cell: ({ value }: CellProps<RowData, string>) => (
             <div className="text-gray-700">{value || <span className="text-gray-300">-</span>}</div>
         ),
     },
@@ -170,7 +162,7 @@ const columns: Column<RowData>[] = [
             </div>
         ),
         accessor: "status",
-        Cell: ({ value } :CellProps<RowData, string>) => {
+        Cell: ({ value }: CellProps<RowData, string>) => {
             if (!value) return <span className="text-gray-300">-</span>;
             return (
                 <span
@@ -204,7 +196,7 @@ const columns: Column<RowData>[] = [
             </div>
         ),
         accessor: "submitter",
-        Cell:({ value } :CellProps<RowData, string>) => (
+        Cell: ({ value }: CellProps<RowData, string>) => (
             <div className="text-gray-700">{value || <span className="text-gray-300">-</span>}</div>
         ),
     },
@@ -221,7 +213,7 @@ const columns: Column<RowData>[] = [
             </div>
         ),
         accessor: "url",
-        Cell:({ value } :CellProps<RowData, string>) => (
+        Cell: ({ value }: CellProps<RowData, string>) => (
             <div className={`text-gray-700 ${value ? "underline" : ""}`}>
                 {value || <span className="text-gray-300">-</span>}
             </div>
@@ -235,7 +227,7 @@ const columns: Column<RowData>[] = [
             </div>
         ),
         accessor: "assigned",
-        Cell: ({ value } :CellProps<RowData, string>) => (
+        Cell: ({ value }: CellProps<RowData, string>) => (
             <div className="text-gray-700">{value || <span className="text-gray-300">-</span>}</div>
         ),
     },
@@ -246,7 +238,7 @@ const columns: Column<RowData>[] = [
             </div>
         ),
         accessor: "priority",
-        Cell:({ value } :CellProps<RowData, string>) => {
+        Cell: ({ value }: CellProps<RowData, string>) => {
             if (!value) return <span className="text-gray-300">-</span>;
             return (
                 <span
@@ -268,21 +260,30 @@ const columns: Column<RowData>[] = [
     {
         Header: () => <div className="flex items-Center  gap-x-2"> Due Date</div>,
         accessor: "dueDate",
-        Cell:({ value } :CellProps<RowData, string>) => (
+        Cell: ({ value }: CellProps<RowData, string>) => (
             <div className="text-gray-700">{value || <span className="text-gray-300">-</span>}</div>
         ),
     },
     {
-        Header: () => <div className="flex items-Center  gap-x-2"> Est. Value</div>,
+        Header: () => <div className="flex items-center  gap-x-2 w-[150px] "> Est. Value</div>,
         accessor: "estValue",
-        Cell:({ value } :CellProps<RowData, string>) => (
-            <div className="text-gray-700">{value || <span className="text-gray-300">-</span>}</div>
+        Cell: ({ value }: CellProps<RowData, string>) => (
+            <div className="text-gray-700">
+                {value ? (
+                    <span>
+                        {value}
+                        <span className="text-gray-200 ml-3 ">₹</span>
+                    </span>
+                ) : (
+                    <span className="text-gray-300">-</span>
+                )}
+            </div>
         ),
     },
     {
         Header: () => <div className="flex items-Center  gap-x-2 w-[130px]"> </div>,
         accessor: "last",
-        Cell: ({ value } :CellProps<RowData, string>) => (
+        Cell: ({ value }: CellProps<RowData, string>) => (
             <div className="text-gray-700 w-[130px]">
                 {value || <span className="text-gray-300">-</span>}
             </div>
@@ -297,10 +298,10 @@ const SpreadsheetGrid: React.FC = () => {
     });
     return (
         <div className=" flex-1 overflow-x-auto w-[100%] scrollbar-hidden">
-            <div className=" flex w-full">
+            <div className=" flex w-full ">
                 <div className="px-2 py-1 text-sm text-gray-700 truncate border border-[#eeeeee] w-[30px]"></div>
 
-                <div className="flex px-2 py-1 text-sm truncate gap-2 items-center bg-[#e2e2e2] border border-[#eeeeee] content-center w-[650px]">
+                <div className="flex px-2 py-1 text-sm truncate gap-2 items-center bg-[#e2e2e2] border border-[#eeeeee] content-center w-[647px]">
                     <div className="flex items-center gap-1 bg-[#eeeeee] py-1 px-1.5  rounded-md border border-[#eeeeee] cursor-pointer">
                         <IoLinkOutline className="text-blue-700  w-4 text-lg" />
                         <span className="text-gray-500">Q3 Financial Overview</span>
@@ -308,9 +309,15 @@ const SpreadsheetGrid: React.FC = () => {
                     <FaArrowsRotate className="w-4 h-4 text-red-500" />
                 </div>
 
-                <div className="px-2 py-1 truncate w-[150px] text-sm text-gray-700 border border-[#eeeeee]"></div>
+                <div
+                    className="px-2 py-1 truncate  text-sm text-gray-700 border border-[#eeeeee]"
+                    style={{ width: "154px" }}
+                ></div>
 
-                <div className="flex items-center justify-center gap-1 py-1 px-2 text-sm bg-[#d2e0d4] text-gray-600 truncate border border-[#eeeeee] cursor-pointer">
+                <div
+                    className="flex items-center justify-center gap-1 py-1 px-2 text-sm bg-[#d2e0d4] text-gray-600 truncate border border-[#eeeeee] cursor-pointer"
+                    style={{ width: "156px" }}
+                >
                     <img src={icon1} alt="ABC" className="w-5 text-gray-300" />
                     <span className="font-[500] tracking-wide">ABC</span>
                     <span
@@ -321,7 +328,10 @@ const SpreadsheetGrid: React.FC = () => {
                     </span>
                 </div>
 
-                <div className="px-2 py-1 text-sm text-gray-600 truncate flex items-center justify-center gap-1 bg-[#DCCFFC] border border-[#eeeeee] cursor-pointer w-[300px]">
+                <div
+                    className="px-2 py-1 text-sm text-gray-600 truncate flex items-center justify-center gap-1 bg-[#DCCFFC] border border-[#eeeeee] cursor-pointer "
+                    style={{ width: "215px" }}
+                >
                     <img src={icon} alt="Answer a question" className="w-5 text-gray-300" />
                     <span className="font-[500] tracking-wide">Answer a question</span>
                     <span className="text-[#afafaf] text-lg tracking-wider ml-2 -translate-1">
@@ -329,7 +339,10 @@ const SpreadsheetGrid: React.FC = () => {
                     </span>
                 </div>
 
-                <div className="px-2 py-1 text-sm text-gray-700 truncate border border-[#eeeeee] flex items-center justify-center bg-[#fac2af] gap-1 cursor-pointer w-[150px]">
+                <div
+                    className="px-2 py-1 text-sm text-gray-700 truncate border border-[#eeeeee] flex items-center justify-center bg-[#fac2af] gap-1 cursor-pointer "
+                    style={{ width: "180px" }}
+                >
                     <img className="w-5" alt="Extract" src={icon} />
                     <span className="font-[500] tracking-wide">Extract</span>
                     <span className="text-[#afafaf] text-lg tracking-wider ml-2 -translate-1">
@@ -337,7 +350,10 @@ const SpreadsheetGrid: React.FC = () => {
                     </span>
                 </div>
 
-                <div className="px-2 py-1 text-sm text-gray-700 truncate border border-[#eeeeee] bg-[#eeeeee] flex items-center justify-center cursor-pointer w-[150px]">
+                <div
+                    className="px-2 py-1 text-sm text-gray-700 truncate border border-[#eeeeee] bg-[#eeeeee] flex items-center justify-center cursor-pointer "
+                    style={{ width: "140px" }}
+                >
                     <IoMdAdd className="w-5 text-3xl" />
                 </div>
             </div>
@@ -347,8 +363,7 @@ const SpreadsheetGrid: React.FC = () => {
                     <div className="px-2 py-1 text-lg  text-[#BCBCBC] bg-gray-100 border border-[#eeeeee] text-center w-[30px]">
                         #
                     </div>
-                    {/* <div className="flex mb-0"> */}
-                    {/* <div className="w-10 mr-2">  */}
+
                     {data.map((_, index) => (
                         <div
                             key={index}
@@ -358,42 +373,59 @@ const SpreadsheetGrid: React.FC = () => {
                         </div>
                     ))}
                 </div>
-                {/* </div> */}
 
-                <table {...getTableProps()} className=" min-w-full border  text-sm">
+                <table {...getTableProps()} className="min-w-full border text-sm">
                     <thead className="bg-gray-100">
-                        {headerGroups.map((headerGroup: HeaderGroup<RowData>) => (
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map((column: ColumnInstance<RowData>) => (
-                                    <th
-                                        {...column.getHeaderProps()}
-                                        className=" relative border p-2 text-left font-semibold text-gray-600 bg-[#F3F3F3] "
-                                    >
-                                        {column.render("Header")}
-                                        <div className="absolute cursor-col-resize top-0 right-0 w-2 h-full"></div>
-                                    </th>
-                                ))}
-                            </tr>
-                        ))}
+                        {headerGroups.map((headerGroup: HeaderGroup<RowData>) => {
+                            const { key: headerKey, ...headerProps } =
+                                headerGroup.getHeaderGroupProps();
+                            return (
+                                <tr key={headerKey} {...headerProps}>
+                                    {headerGroup.headers.map((column: ColumnInstance<RowData>) => {
+                                        const { key: colKey, ...colProps } =
+                                            column.getHeaderProps();
+                                        return (
+                                            <th
+                                                key={colKey}
+                                                {...colProps}
+                                                className="relative border p-2 text-left font-semibold text-gray-600 bg-[#F3F3F3] sticky top-0 z-11"
+                                            >
+                                                {column.render("Header")}
+                                                <div className="absolute cursor-col-resize top-0 right-0 w-2 h-full"></div>
+                                            </th>
+                                        );
+                                    })}
+                                </tr>
+                            );
+                        })}
                     </thead>
                     <tbody {...getTableBodyProps()}>
                         {rows.map((row: Row<RowData>) => {
                             prepareRow(row);
+                            const { key: rowKey, ...rowProps } = row.getRowProps();
                             return (
-                                <tr {...row.getRowProps()} className="hover:bg-gray-50">
-                                    {row.cells.map((cell: Cell<RowData>) => (
-                                        <td
-                                            {...cell.getCellProps()}
-                                            className="border px-4 py-2"
-                                            onClick={() =>
-                                                console.log(
-                                                    `Clicked ${cell.column.id} | Value: ${cell.value}`
-                                                )
-                                            }
-                                        >
-                                            {cell.render("Cell")}
-                                        </td>
-                                    ))}
+                                <tr
+                                    key={rowKey}
+                                    {...rowProps}
+                                    className="hover:bg-gray-50 cursor-pointer"
+                                >
+                                    {row.cells.map((cell: Cell<RowData>) => {
+                                        const { key: cellKey, ...cellProps } = cell.getCellProps();
+                                        return (
+                                            <td
+                                                key={cellKey}
+                                                {...cellProps}
+                                                className="border px-4 py-2"
+                                                onClick={() =>
+                                                    console.log(
+                                                        `Clicked ${cell.column.id} | Value: ${cell.value}`
+                                                    )
+                                                }
+                                            >
+                                                {cell.render("Cell")}
+                                            </td>
+                                        );
+                                    })}
                                 </tr>
                             );
                         })}
